ce3f21812243e822427589333c30809be7b4c354
ruben-mar <hello@rubenmartinez.me>
2020-11-15 00:38:48 +0100

    Completed a fully operational script

--- process_images.py
+++ process_images.py
@@ -1,7 +1,6 @@
 # Rename Olympus files with their EXIF date
 """
-Script to rename image files with their EXIF date
-It renames file with the extensions JPG and ORF (Unprocessed RAW photo taken with an Olympus digital camera)
+The script renames files with the extensions JPG and ORF (Unprocessed RAW photo taken with an Olympus digital camera)
 The date is not extracted from the ORF file (see https://exiftool.org/TagNames/Olympus.html)
 """
 
@@ -24,47 +23,11 @@ def show_menu():
 
 
 def list_files(path) -> list:
-    if os.path.exists(path):
-        candidates = sorted(os.listdir(path))
-        # Change to the target directory in the same way as the UNIX cd command.
-        os.chdir(path) 
-    return candidates
+    files = sorted(os.listdir(path))
+    # Change to the target directory in the same way as the UNIX cd command.
+    os.chdir(path) 
+    return files
 
-
-def isImage(file) -> bool : # the filename is an Image object
-    with open(file, 'rb') as image_file:
-        try:
-            my_image = Image(image_file)
-            my_image.has_exif
-            return True
-        except:
-            return False
-
-
-def pairImages(files):
-    images =[]
-    enum = enumerate(files)
-    for e in enum:
-        filename = e[1].split('.') # split() returns a list
-        # Is Image and the filename doesn't contain more than one dot?
-        if isImage(e[1]) and len(filename) < 3:
-            for file in files:
-                if re.match(rf"{filename[0]}.[Oo][Rr][Ff]$", file):
-                    el = list(e) # turn the tuple to list so it can append elements
-                    el.append(file) # add the raw image
-                    images.append(el) # add the compressed image
-    return images
-
-path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/pics: ")
-
-files = list_files(path)
-
-paired = pairImages(files)
-
-print(paired)
-
-
-"""
 
 def show_files(files, path):
     fslashes = len([key for key, val in enumerate(path) if val in set(["/"])]) 
@@ -77,31 +40,77 @@ def show_files(files, path):
     print('[...]')
 
 
+def isImage(file) -> bool : # the file is an Image object
+    with open(file, 'rb') as image_file:
+        try:
+            my_image = Image(image_file)
+            return my_image.has_exif
+        except:
+            return False
+
+
+def extensionORF(pattern,string) -> bool: # the file has an orf extension
+    return re.match(rf"{pattern}.[Oo][Rr][Ff]$",string)
+
+
+def classifyFiles(files) -> tuple:
+    compressed, raw = [], []
+    for file in files:
+        filename = file.split('.') # split() returns a list
+        # Is Image and the filename doesn't contain more than one dot?
+        if isImage(file) and len(filename) < 3:
+            compressed.append(file)
+        elif extensionORF(filename[0],file):
+            raw.append(file) # add the compressed image
+    return compressed, raw
+
+
+def listCommonName(jpgs,orfs) -> list:
+    paired = []
+    filenames = []
+    for jpg in jpgs:
+        filenames.append(jpg.split('.')[0])
+    for raw in orfs:
+        if raw.split('.')[0] in filenames:
+            paired.append(raw.split('.')[0])
+    return paired # a lisf of filenames without extension that have compressed and raw files
+
+
+def remove_unpaired(paired, files):
+    for file in files:
+        if file.split('.')[0] not in paired:
+            try:
+                os.remove(file)
+            except:
+                print("Couldn't delete file {}".format(file))
+
+
 def rename_files(jpgs, orfs):
     count = 0
     for jpg in jpgs:
         filename = jpg.split('.',maxsplit=1)[0]
         tagged = Image(jpg)
         new_name = tagged.datetime_original.replace(":", "").replace(" ","_")
-        if new_name != '0000-00-00 00:00:00.0000':
-            new_jpg_name = new_name + '.jpg'
-            if jpg != new_jpg_name and new_jpg_name != '0000-00-00 00:00:00.0000':
-                os.rename(jpg,new_jpg_name)
+        new_jpg_name = new_name + '.jpg'
+        if jpg != new_jpg_name and new_jpg_name != '0000-00-00 00:00:00.0000':
+            os.rename(jpg,new_jpg_name)
+            count += 1
+            print(jpg ,' > ', new_jpg_name)
+        orf = filename + '.' +'orf' 
+        if orf in orfs:
+            new_orf_name = new_name + '.orf'
+            if orf != new_orf_name:
+                os.rename(orf,new_orf_name)
                 count += 1
-                print(jpg ,' > ', new_jpg_name)
-            orf = filename + '.' +'orf' 
-            if orf in orfs:
-                new_orf_name = new_name + '.orf'
-                if orf != new_orf_name:
-                    os.rename(orf,new_orf_name)
-                    count += 1
-                    print(orf ,' > ', new_orf_name)
+                print(orf ,' > ', new_orf_name)
     return count 
 
 # MAIN
 
 # Ask where the folder with the files is.
 path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/pics: ")
+while not os.path.exists(path):
+    path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/pics: ")
 
 option = show_menu()
 
@@ -111,13 +120,14 @@ while option != '0':
         show_files(files,path)
     elif option == '2':
         print("\nThere are {} files.\n".format(len(files)))
-        if remove_unpaired(find_mismatch(classify_by_extension(files))):
+        matched = listCommonName(classifyFiles(files)[0], classifyFiles(files)[1])
+        if remove_unpaired(matched,files):
             print("\nAll the ORF files without equivalent JPEG were deleted.")
         else:
             print("\nAll the raw ORF files have compressed JPEG versions. No raw file to remove.")
     elif option == '3':
         print("\nThere are {} files.\n".format(len(files)))
-        res = rename_files(classify_by_extension(files)[0], classify_by_extension(files)[1])
+        res = rename_files(classifyFiles(files)[0], classifyFiles(files)[1])
         if res > 0:
             print("\nRenamed {} files with their EXIF dates.".format(res))
         else:
@@ -127,4 +137,3 @@ while option != '0':
     option = show_menu()        
 
 print("\nBye.\n")
-"""
