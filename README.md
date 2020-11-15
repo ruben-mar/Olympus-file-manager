155558554e8eec90446d7cbb78f9dbe727457f73
ruben-mar <hello@rubenmartinez.me>
2020-11-15 18:43:59 +0100

    Clean up, readme and changelog prior to release of version two

--- README.md
+++ README.md
@@ -1,42 +1,59 @@
+README
+
 # Olympus-file-manager
-This application renames batches of ORF files with the date and time of the creation of the picture. The date in the files names allow the storage of massive amounts of images recorded in RAW mode in the same folder, easy to sort and select
+
+This application renames batches of ORF files with the date and time of the creation of the picture. The date in the file names allow the storage of massive amounts of images recorded in RAW mode in the same folder, easy to sort and select.
 
 [![Hex.pm](https://img.shields.io/hexpm/l/plug.svg)](/LICENSE.md)
 
-# 1. Overview
+# 1\. Overview
 
-## 1.a. RAW files and their JPEG equivalent
+## 1.1. RAW files and their JPEG equivalent
 
 Olympus cameras save their loss less compressed RAW pictures in a ORF file format. The size of ORF files is approx. 17.3 MB. The size of their equivalent lossy compressed JPEG ones is rarely larger than 10 MB (even for counts of pixels larger than 3200 x 2400 and compression rates of 1/2.7).
 
+## 1.2. Exploring your photos and managing JPEG and ORF files
 
-## 1.b. Managing JPEG and ORF files
+The imported JPEG image files from Olympus digital cameras are easier to browse than the heavier ORF ones because computers load JPEG files faster.
 
-The imported JPEG image files from Olympus digital cameras are easier to browse than the heavier ORF ones because computers load JPEG files faster. 
+Image viewers allow the removal of consecutive JPEG files as you examine the content of the folder containing the imported images. The deletion of JPEG files leaves their ORF equivalent in the folder. These orphan ORF files can be deleted.
 
-Image viewers allow the removal of consecutive JPEG files as you examine the content of the folder containing the imported images. The deletion of JPEG files leaves their ORF equivalent in the folder.
+## 1.3. File names of Olympus cameras
 
-## 1.c. File names of Olympus cameras
+Olympus cameras name the image files with as **Pmdd0000.jpg** ("P" for sRGB color space) where 'm' is month and 'dd' day, e.g. P1260078.JPG and P1260078.ORF.
 
-Olympus cameras name the image files with as Pmdd0000.jpg (for sRGB color space) where 'm' is month and 'dd' day, e.g. P1260078.JPG and P1260078.ORF.
+Read more at [Olympus file name syntax](https://en.wikipedia.org/wiki/ORF_format#File_name_syntax).
 
-Read more at [Olympus file name syntax](https://en.wikipedia.org/wiki/ORF_format#File_name_syntax)).
+This script manages extensions .jpg in lower and upper case like .JPG and even files with missing extensions.
 
-# 2. Get started
+# 2\. Get started
 
 This application removes orphaned ORF files whose JPEG equivalent ones where removed while browing the folder in a GUI application and renames the remaing JPEG and ORF files with the timestamp of the creation of the image.
 
+## 2.1. Requirements
+
 This application is written in Python so it runs successfully on the following operating systems:
 
-- _Linux_
-- _Windows_
-- _Mac OS_
+- *Linux*
+- *Windows*
+- *Mac OS*
 
-as long as they have Python installed.
+as long as they have Python >=3.6 and the project [exif](https://pypi.org/project/exif/) installed.
 
-# 3. Develop
+> pip install exif
 
-This application requires Python >=3.6 and the project [exif 1.0.0](https://pypi.org/project/exif/).
+## 2.2. How to use
+
+Import the files from your Olympus camera into your computer. The script removes files that are not EXIF tagged files or that that don't have JPG or ORF extensions. 
+
+Run the script from the command line
+
+> python3 process_images.py
+
+## 2.3. Disclaimer
+
+For better performance you should create a folder specifically for your import with only the imported files from your Olympus camera. The author of this script releases it as is, without any warranty of its correct operation. Back up your files before executing the script. 
+
+# 3\. Develop
+
 - **Found issues?** Then please **file an issue** [here](https://github.com/ruben-mar/Olympus-file-manager/issues).
-
-
--- process_images.py
+++ process_images.py
@@ -1,13 +1,13 @@
-# Rename Olympus files with their EXIF date
+# Rename Olympus files with their EXIF date and remove raw images without compressed equivalent
 """
-The script renames files with the extensions JPG and ORF (Unprocessed RAW photo taken with an Olympus digital camera)
-The date is not extracted from the ORF file (see https://exiftool.org/TagNames/Olympus.html)
+The script removes raw images missing their compressed versions and it renames files with the extensions JPG and ORF. The date is not extracted from the ORF file but this script adds its to the raw files by taking it from the compressed one.
 """
 
 import os
 import re
 import fnmatch
 from itertools import islice
+from time import perf_counter
 from exif import Image # https://pypi.org/project/exif/ It cannot process .ORF files
 
 MAX = 5 # Limit of number of files to list in the output
@@ -15,8 +15,8 @@ MAX = 5 # Limit of number of files to list in the output
 def show_menu():
     print()
     print("Enter 1 to see the list of files.")
-    print("Enter 2 to remove orphan ORF files.")
-    print("Enter 3 to rename files with their EXIF date.")
+    print("Enter 2 to rename files with their EXIF date.")
+    print("Enter 3 to remove orphan ORF files.")
     print("Enter 0 to exit the application.")    
     option = input("\nEnter your option: ")
     return option
@@ -31,12 +31,12 @@ def list_files(path) -> list:
 
 def show_files(files, path):
     fslashes = len([key for key, val in enumerate(path) if val in set(["/"])]) 
-    print("\nThere are {} files in [...]/{}. These are the first {}:\n".format(len(files),path.split("/")[fslashes],MAX))
+    print("\nThere are {} files in [...]/{}. These are the first {}:".format(len(files),path.split("/")[fslashes],MAX))
     count = 0
     while count < MAX:
-        for i in islice(files, 0, MAX):
+        for i in islice(enumerate(files), 0, MAX):
             count += 1
-            print(i)
+            print(i[0]+1,': ',i[1], sep="")
     print('[...]')
 
 
@@ -57,8 +57,8 @@ def classifyFiles(files) -> tuple:
     compressed, raw = [], []
     for file in files:
         filename = file.split('.') # split() returns a list
-        # Is Image and the filename doesn't contain more than one dot?
-        if isImage(file) and len(filename) < 3:
+        # Is Image object as per the module exif
+        if isImage(file):
             compressed.append(file)
         elif extensionORF(filename[0],file):
             raw.append(file) # add the compressed image
@@ -76,16 +76,7 @@ def listCommonName(jpgs,orfs) -> list:
     return paired # a lisf of filenames without extension that have compressed and raw files
 
 
-def remove_unpaired(paired, files):
-    for file in files:
-        if file.split('.')[0] not in paired:
-            try:
-                os.remove(file)
-            except:
-                print("Couldn't delete file {}".format(file))
-
-
-def rename_files(jpgs, orfs):
+def rename_files(jpgs, orfs) ->  int:
     count = 0
     for jpg in jpgs:
         filename = jpg.split('.',maxsplit=1)[0]
@@ -95,22 +86,34 @@ def rename_files(jpgs, orfs):
         if jpg != new_jpg_name and new_jpg_name != '0000-00-00 00:00:00.0000':
             os.rename(jpg,new_jpg_name)
             count += 1
-            print(jpg ,' > ', new_jpg_name)
+            print(jpg ,' renamed as ', new_jpg_name)
         orf = filename + '.' +'orf' 
         if orf in orfs:
             new_orf_name = new_name + '.orf'
             if orf != new_orf_name:
                 os.rename(orf,new_orf_name)
                 count += 1
-                print(orf ,' > ', new_orf_name)
+                print(orf ,' renamed to ', new_orf_name)
     return count 
+
+
+def remove_unpaired(files, compressed, paired) -> int:
+    count = 0
+    for file in files:
+        if file.split('.')[0] not in paired and file not in compressed:
+            try:
+                os.remove(file)
+                count += 1
+            except:
+                print("Couldn't delete file {}".format(file))
+    return count
 
 # MAIN
 
 # Ask where the folder with the files is.
-path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/pics: ")
+path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/downloads: ")
 while not os.path.exists(path):
-    path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/pics: ")
+    path = input("Enter the location of the folder containing the Olympus files, for instance /home/user/downloads: ")
 
 option = show_menu()
 
@@ -119,19 +122,24 @@ while option != '0':
     if option == '1':
         show_files(files,path)
     elif option == '2':
-        print("\nThere are {} files.\n".format(len(files)))
-        matched = listCommonName(classifyFiles(files)[0], classifyFiles(files)[1])
-        if remove_unpaired(matched,files):
-            print("\nAll the ORF files without equivalent JPEG were deleted.")
-        else:
-            print("\nAll the raw ORF files have compressed JPEG versions. No raw file to remove.")
-    elif option == '3':
-        print("\nThere are {} files.\n".format(len(files)))
+        start = perf_counter()
+        count_before = len(list_files(path))
         res = rename_files(classifyFiles(files)[0], classifyFiles(files)[1])
         if res > 0:
-            print("\nRenamed {} files with their EXIF dates.".format(res))
+            end = perf_counter()
+            print("\nRenamed {} files with their EXIF dates in {} seconds.".format(res, round(end-start,1)))
         else:
             print("\nNo file renamed.")
+    elif option == '3':
+        start = perf_counter()
+        count_before = len(list_files(path))
+        matched = listCommonName(classifyFiles(files)[0], classifyFiles(files)[1])
+        if remove_unpaired(files, classifyFiles(files)[0], matched) > 0:
+            count_after = len(list_files(path))
+            end = perf_counter()
+            print("\n{} ORF files without equivalent JPG were deleted in {} seconds.".format(count_before-count_after,round(end-start,1)))
+        else:
+            print("\nAll the raw ORF files have compressed JPG versions. No raw file to remove.")
     else:
         print("\nWrong option.")
     option = show_menu()        
